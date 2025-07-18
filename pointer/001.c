8a20162c7544eb6cb62e0abe7009c9a15ec8451f
elkenrain <rainy.general@gmail.com>
2025-07-18 19:09:42 +0700

    Create 001.c

---
+++ pointer/001.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int m = 10;
+    int n, o;
+    int *z;
+
+    z = &m;
+
+    printf("Pointer : Show the basic declaration of pointer :\n");
+    printf("----------------------------------------------------\n");
+    printf("Here is m = %d, n and o are two integer variable and *z is an integer\n", m);
+    printf("z stores the address of m = %p\n", z);
+    printf("*z stores the value of m = %d\n", *z);
+    printf("&m is the address of m = %p\n", &m);
+    printf("&n stores the address of n = %p\n",&n);
+    printf("&o stores the address of o = %p\n", &o);
+    printf("&z stores the address of z = %p\n", &z);
+
+    return 0;
+}
